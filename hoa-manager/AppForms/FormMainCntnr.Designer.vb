c6ce2bc80107ad453b474027610e2492735d9ca8
poolace <matthew.wolf.70@gmail.com>
2020-05-19 11:57:26 -0400

    Small Menu update and lots of database stuff

--- hoa-manager/AppForms/FormMainCntnr.Designer.vb
+++ hoa-manager/AppForms/FormMainCntnr.Designer.vb
@@ -22,18 +22,60 @@ Partial Class FormMainCntnr
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.FMC_MenuStrip = New System.Windows.Forms.MenuStrip()
+        Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TSMI_FMC_File_New = New System.Windows.Forms.ToolStripMenuItem()
+        Me.TSMI_FMC_File_Open = New System.Windows.Forms.ToolStripMenuItem()
+        Me.FMC_MenuStrip.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'FMC_MenuStrip
+        '
+        Me.FMC_MenuStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FileToolStripMenuItem})
+        Me.FMC_MenuStrip.Location = New System.Drawing.Point(0, 0)
+        Me.FMC_MenuStrip.Name = "FMC_MenuStrip"
+        Me.FMC_MenuStrip.Size = New System.Drawing.Size(800, 24)
+        Me.FMC_MenuStrip.TabIndex = 1
+        Me.FMC_MenuStrip.Text = "FMC_MenuStrip"
+        '
+        'FileToolStripMenuItem
+        '
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.TSMI_FMC_File_New, Me.TSMI_FMC_File_Open})
+        Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
+        Me.FileToolStripMenuItem.Size = New System.Drawing.Size(37, 20)
+        Me.FileToolStripMenuItem.Text = "&File"
+        '
+        'TSMI_FMC_File_New
+        '
+        Me.TSMI_FMC_File_New.Name = "TSMI_FMC_File_New"
+        Me.TSMI_FMC_File_New.Size = New System.Drawing.Size(180, 22)
+        Me.TSMI_FMC_File_New.Text = "&New"
+        '
+        'TSMI_FMC_File_Open
+        '
+        Me.TSMI_FMC_File_Open.Name = "TSMI_FMC_File_Open"
+        Me.TSMI_FMC_File_Open.Size = New System.Drawing.Size(180, 22)
+        Me.TSMI_FMC_File_Open.Text = "&Open"
         '
         'FormMainCntnr
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.FMC_MenuStrip)
         Me.IsMdiContainer = True
+        Me.MainMenuStrip = Me.FMC_MenuStrip
         Me.Name = "FormMainCntnr"
         Me.Text = "HOA Manager"
+        Me.FMC_MenuStrip.ResumeLayout(False)
+        Me.FMC_MenuStrip.PerformLayout()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
 
+    Friend WithEvents FMC_MenuStrip As MenuStrip
+    Friend WithEvents FileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents TSMI_FMC_File_New As ToolStripMenuItem
+    Friend WithEvents TSMI_FMC_File_Open As ToolStripMenuItem
 End Class
--- hoa-manager/AppForms/FormMainCntnr.vb
+++ hoa-manager/AppForms/FormMainCntnr.vb
@@ -2,4 +2,8 @@
     Private Sub FormMainCntnr_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         'Made a change
     End Sub
+
+    Private Sub TSMI_FMC_File_New_Click(sender As Object, e As EventArgs) Handles TSMI_FMC_File_New.Click
+
+    End Sub
 End Class
